a513d0ba23ab716315e055a58d07c34b585bf532
blacksnail789521 <blacksnail789521@gmail.com>
2021-01-15 14:32:48 +0800

    move cheat sheet to evernote

--- Cheat_Sheet/Git/alias/useful_alias.sh
+++
@@ -1,6 +0,0 @@
-alias gitroot='cd "$(git rev-parse --show-toplevel)"'
-alias gitpull='git pull --rebase --autostash'
-alias gitcommit='git commit -m '
-alias gitstatus='git status'
-alias gitlog='git log --oneline -5'
-alias gitadd='git add .'
