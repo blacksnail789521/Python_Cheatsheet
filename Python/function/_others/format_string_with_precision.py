365ca981aaf78aaa20c8fc2da9a805e3e5c2dba9
blacksnail789521 <blacksnail789521@gmail.com>
2021-02-01 12:16:07 +0800

    add money format

--- Python/function/_others/format_string_with_precision.py
+++ Python/function/_others/format_string_with_precision.py
@@ -11,3 +11,15 @@ print(f"{'left aligned':<30}")
 print(f"{'right aligned':>30}")
 print(f"{'centered':^30}")
 print(f"{'centered':*^30}") # Use "*" as a fill char.
+
+# two columns (money format)
+saving = 967530
+stock = 44690106
+interest = stock * 1.1
+
+float_precision = 0
+left_align = 10
+right_align = 10
+print(f"{'saving:':<{left_align}}{saving:>{right_align},.{float_precision}f}")
+print(f"{'stock:':<{left_align}}{stock:>{right_align},.{float_precision}f}")
+print(f"{'interest:':<{left_align}}{interest:>{right_align},.{float_precision}f}")
