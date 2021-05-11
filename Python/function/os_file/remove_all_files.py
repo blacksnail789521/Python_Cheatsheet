104567ef14969c3fe487b9423ed22a84e0c8a07b
blacksnail789521 <blacksnail789521@gmail.com>
2021-05-11 15:48:06 +0800

    add one line for shutil.rmtree

--- Python/function/os_file/remove_all_files.py
+++ Python/function/os_file/remove_all_files.py
@@ -8,3 +8,6 @@ remove_path = os.path.join(exec_path, "output")
 if os.path.exists(remove_path):
     print("Remove!")
     shutil.rmtree(remove_path)
+
+# One line
+shutil.rmtree(remove_path, ignore_errors = True)
