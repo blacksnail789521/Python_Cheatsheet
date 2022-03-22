b8a842600c6117ae03dfba4ac0c34394a1f970a6
blacksnail789521 <blacksnail789521@gmail.com>
2022-03-22 14:57:03 +0800

    add metavar in argparse

--- Python/function/argparse/argparse_example.py
+++ Python/function/argparse/argparse_example.py
@@ -5,14 +5,15 @@ parser = argparse.ArgumentParser(description='calculate X to the power of Y and 
 # Exclusive group
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-group.add_argument('-q', '--quiet', action='store_true') # action --> act as a flag
+group.add_argument('-q', '--quiet', action='store_true') # action --> act as a flag (Don't use 'metavar')
 
 # Positional arguments
 parser.add_argument('x', type=int, help='the base')
-parser.add_argument('y', type=int, help='the exponent', choices = [0, 1, 2])
+parser.add_argument('y', type=int, help='the exponent', choices = [0, 1, 2], default = 2)
 
 # Optional arguments
-parser.add_argument('-b', '--bias', type=int, help='the bias', choices = [0, 1, 2, 3], default=3)
+parser.add_argument('-b', '--bias', type=int, help='the bias') # metavar = 'BIAS'
+parser.add_argument('--batch_size', type=int, help='the batch_size (not use in this case)', metavar='N')
 
 
 '''----------------------------------------------------------------------'''
@@ -28,7 +29,7 @@ else:
     print(f'{args.x}^{args.y}+{args.bias} == {answer}')
 
 '''
-usage: argparse_example.py [-h] [-v | -q] [-b {0,1,2,3}] x {0,1,2}
+usage: argparse_example.py [-h] [-v | -q] [-b BIAS] [--batch_size N] x {0,1,2}
 
 calculate X to the power of Y and add a bias
 
@@ -40,6 +41,6 @@ optional arguments:
   -h, --help            show this help message and exit
   -v, --verbose         increase output verbosity
   -q, --quiet
-  -b {0,1,2,3}, --bias {0,1,2,3}
-                        the bias
+  -b BIAS, --bias BIAS  the bias
+  --batch_size N        the batch_size (not use in this case)
 '''
