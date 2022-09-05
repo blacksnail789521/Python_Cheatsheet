68cba51c9d9b00115efff4a31da2c32a71ef1b82
blacksnail789521 <blacksnail789521@gmail.com>
2022-09-05 22:08:40 +0800

    add basic examples in argparse

--- Python/function/argparse/argparse_example.py
+++ Python/function/argparse/argparse_example.py
@@ -1,33 +1,33 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='calculate X to the power of Y and add a bias',
+parser = argparse.ArgumentParser(description='argparse_example',
                                  formatter_class = argparse.ArgumentDefaultsHelpFormatter) # Show default value
+
+# Positional arguments (must given)
+parser.add_argument('x', type=int, help='the base')
+parser.add_argument('y', type=int, help='the exponent', choices = [0, 1, 2], default = 2)
+
+# Optional arguments
+parser.add_argument('-b', '--bias', type=int, help='the bias') # metavar = 'BIAS', default = None
+parser.add_argument('--batch_size', type=int, help='the batch_size (not use in this case)', metavar='N')
+parser.add_argument('--enable_bn', type=bool, help='enable batch normalization', default = True)
+parser.add_argument('--model_name', type=str, help='model name', default = 'DNN')
+parser.add_argument('-l1','--list1', nargs='+', help='set all elements in a list', default = ['0'])   # python arg.py -l1 1 2 3 4
+parser.add_argument('-l2','--list2', action='append', help='add element one by one', default = ['0']) # python arg.py -l2 1 -l2 2 -l2 3 -l2 4
+
 
 # Exclusive group
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
 group.add_argument('-q', '--quiet', action='store_true') # action --> act as a flag (Don't use 'metavar')
 
-# Positional arguments
-parser.add_argument('x', type=int, help='the base')
-parser.add_argument('y', type=int, help='the exponent', choices = [0, 1, 2], default = 2)
-
-# Optional arguments
-parser.add_argument('-b', '--bias', type=int, help='the bias') # metavar = 'BIAS'
-parser.add_argument('--batch_size', type=int, help='the batch_size (not use in this case)', metavar='N')
-
-
 '''----------------------------------------------------------------------'''
-
+# Assume we use `python argparse_example.py 3 2`
 args = parser.parse_args()
-answer = args.x**args.y+args.bias
-
-if args.quiet:
-    print(answer)
-elif args.verbose:
-    print(f'{args.x} to the power {args.y} adding {args.bias} equals {answer}')
-else:
-    print(f'{args.x}^{args.y}+{args.bias} == {answer}')
+print(args)
+'''
+Namespace(batch_size=None, bias=None, list1=None, list2=None, quiet=False, verbose=False, x=3, y=2)
+'''
 
 
 # Use `python argparse_example.py -h` to get the help message.
