cddda683dce9fbfa66d4a126b4a951ea11de59ce
blacksnail789521 <blacksnail789521@gmail.com>
2023-01-28 13:20:39 +0800

    all combinations with a good example

--- list/all_combinations_of_list_of_lists.py
+++ list/all_combinations_of_list_of_lists.py
@@ -1,6 +1,7 @@
 import itertools
 
-list_of_lists = [[1,2],[3,4]]
+list_of_lists = [[4, 8, 16], ["a", "b"]]
 
 # Get all combinations of a list of lists
-products = list(itertools.product(*list_of_lists)) # [(1, 3), (1, 4), (2, 3), (2, 4)]
+products = list(itertools.product(*list_of_lists))
+print(products)  # [(4, 'a'), (4, 'b'), (8, 'a'), (8, 'b'), (16, 'a'), (16, 'b')]
