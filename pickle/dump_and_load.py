9a2732dd163a25b8e867f8d1935cde6e1512200e
blacksnail789521 <blacksnail789521@gmail.com>
2023-02-14 14:18:21 +0800

    use pkl as extension

--- pickle/dump_and_load.py
+++ pickle/dump_and_load.py
@@ -2,14 +2,14 @@ import pickle
 import pandas as pd
 
 
-before = {"1": "XD", "2": "HAHA", "3": pd.DataFrame()}
+before = {"1": "XD", "2": 2, "3": pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})}
 
 # Dump.
-with open("data.pickle", "wb") as pickle_file:
+with open("data.pkl", "wb") as pickle_file:
     pickle.dump(before, pickle_file, pickle.HIGHEST_PROTOCOL)
 
 # Load.
-with open("data.pickle", "rb") as pickle_file:
+with open("data.pkl", "rb") as pickle_file:
     after = pickle.load(pickle_file)
 
 print(before)
